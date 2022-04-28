5d93cfe9cdd14477ba7f3939dff1838990590b68
fiveFloorMan <harvieusegithub@gmail.com>
2022-04-28 16:34:22 +0800

    add hanlebars@4.0.2

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
 
 require('./config/mongoose')
 
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-  res.send(`check-users is running`)
+  res.render('index')
 })
 
 app.listen(PORT, () => {
