536fb20b361402ff667a9fbbb6592df38753891a
fiveFloorMan <harvieusegithub@gmail.com>
2022-04-28 17:53:27 +0800

    驗證身分功能完成

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const router = express.Router
+const router = express.Router()
 
 const login = require('./modules/login')
 const home = require('./modules/home')
--- routes/modules/login.js
+++ routes/modules/login.js
@@ -1,6 +1,7 @@
 // url = localhost:3000/
 const express = require('express')
-const router = express.Router
+const router = express.Router()
+const Users = require('../../models/user')
 
 // login 頁面
 router.get('/', (req, res) => {
@@ -9,7 +10,18 @@ router.get('/', (req, res) => {
 
 // 接login回傳的資料
 router.post('/', (req, res) => {
-
+  const email = req.body.email
+  const password = req.body.password
+  Users.findOne({ email, password })
+    .lean()  
+    .then(user => {
+      if(user === null){
+        return console.log(`email or password is wrong`)
+      }
+      console.log(`this man exist`)
+      res.render('index',{value : user.name})
+    })
+    .catch(error => console.log(error))
 })
 
 module.exports = router
